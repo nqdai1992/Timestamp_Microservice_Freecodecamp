cbefddf465019a8d62464dad370ab5410c4df5f3
nqdai1992 <nguyendai1992@gmail.com>
2017-04-17 02:19:48 +0700

    Complete first app

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 /**
  * Created by Dainguyen on 16/04/2017.
  */
+var express = require('express');
+var app = express();
+app.get('/', (req,res)=>{
+    res.end('Hello world');
+});
+app.listen(process.argv[2]||3000);
